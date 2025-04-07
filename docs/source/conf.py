e26f14b039ef81c4f60afe3ee544ce44e38c6642
Ruancun Li <liruancun@pku.edu.cn>
2025-04-07 20:05:50 +0800

    doc
    create doc

---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GalfitS'
+copyright = '2025, Li R. & Ho C. Luis'
+author = 'Li R. & Ho C. Luis'
+release = 'Public 1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon', # for Google/Numpy style docstrings
+]
+
+html_theme = 'sphinx_rtd_theme'
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
